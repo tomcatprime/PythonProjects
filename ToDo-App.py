2a211d40972f2785b7735e009810c07a86507dee
Chruszewski <karolchru@gmail.com>
2023-11-06 17:10:29 +0200

    first commit

---
+++ ToDo-App.py
@@ -0,0 +1,27 @@
+todos =[]
+
+while True:
+    user_action = input("Type add, show, edit, or exit:")
+    user_action = user_action.strip()
+    
+    match user_action:
+        case 'add':
+            todo = input("Enter a todo: ")
+            todos.append(todo)
+        case 'show' | 'display':
+            for index, item in enumerate(todos):
+                row = f"{index + 1}-{item}"
+                print(row)
+        case 'edit':
+            number = int(input("Number of the todo to edit: "))
+            number  = number - 1
+            new_todo = input("Enter new todo: ")
+            todos[number] = new_todo
+            
+        case 'exit':
+            break
+        case not_match:
+            print("Hey, you entered and unknown command")
+            
+print("Bye!")
+
