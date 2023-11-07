bffd3beb928b53dbca146c8e8dc8253a8d45a76d
Chruszewski <karolchru@gmail.com>
2023-11-07 16:31:06 +0200

    modify show and add section

--- ToDo-App.py
+++ ToDo-App.py
@@ -6,14 +6,23 @@ while True:
     
     match user_action:
         case 'add':
-            todo = input("Enter a todo: ")
+            todo = input("Enter a todo: ") + "\n"
+            file = open('todos.txt', r)
+            todos = file.readlines()
+            file.close()
             todos.append(todo)
+            
             file = open('todos.txt', 'w')
             file.writelines(todos)
+            file.close()
         case 'show' | 'display':
+            file = open('todos.txt', 'r')
+            todos = file.readlines()
+            file.close()
             for index, item in enumerate(todos):
                 row = f"{index + 1}-{item}"
                 print(row)
+        
         case 'edit':
             number = int(input("Number of the todo to edit: "))
             number  = number - 1
